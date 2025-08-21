2ff4a68c664e2416c5b6e2940a65e7ca52bdf828
Abi Chang <abichangtw@gmail.com>
2025-08-21 22:03:27 +0800

    feat: setup Spring Boot project

---
+++ src/test/java/com/abicoding/jcconf/speed_up_springbootest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abicoding.jcconf.speed_up_springbootest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
